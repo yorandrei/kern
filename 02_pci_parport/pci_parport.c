ebed826b530fac9af4b5d02ed1b04d8ff3834c91
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2022-03-09 14:49:37 -0800

    PCI device opening

--- 02_pci_parport/pci_parport.c
+++ 02_pci_parport/pci_parport.c
@@ -7,14 +7,36 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Andrei");
 MODULE_DESCRIPTION("A simple LKM for testing a PCIe to parallel port adapter");
 
-#define VENDOR_ID
-#define DEVICE_ID
+#define VENDOR_ID 0x1c00
+#define DEVICE_ID 0x3050
+
+/** PCI device */
+static struct pci_dev *ptr; 
 
 /**
  * @brief This function is called when the module is loaded into the kernel
  */
 static int __init ModuleInit(void) {
-    printk("Hello, kernel!\n");
+    u16 val;
+
+    /* Check if a PCI device with vendor and device id is available */
+    ptr = pci_get_device(VENDOR_ID, DEVICE_ID, ptr);
+    if(ptr == NULL) {
+        printk("pci_parport - PCI to parport adapter is not available!\n");
+        return -1;
+    }
+
+    /* Enable the device */
+    if(pci_enable_device(ptr) < 0) {
+        printk("pci_parport - Could not enable the device!\n");
+        return -1;
+    }
+
+    /* Access config space */
+    pci_read_config_word(ptr, PCI_VENDOR_ID, &val);
+    printk("pci_parport - VENDOR ID: 0x%x\n", val);
+    pci_read_config_word(ptr, PCI_DEVICE_ID, &val);
+    printk("pci_parport - VENDOR ID: 0x%x\n", val);
     return 0;
 }
 
@@ -22,7 +44,7 @@ static int __init ModuleInit(void) {
  * @brief This function is called when the module is removed from the kernel
  */
 static void __exit ModuleExit(void) {
-    printk("Goodbye, kernel\n");
+    printk("pci_parport - Unload Module\n");
 }
 
 module_init(ModuleInit);
